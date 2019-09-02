cd22474a678dee6de0a1deea6ad799a77b316c8c
zheng <765324639@qq.com>
2019-09-03 00:24:10 +0800

    add simple icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { SwiperComponent } from './swiper/swiper.component';
+import { IconsComponent } from './icons/icons.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    SwiperComponent
+    SwiperComponent,
+    IconsComponent
   ],
   imports: [
     BrowserModule,
--- src/app/image.ts
+++ src/app/image.ts
@@ -1,4 +1,5 @@
 export class Image {
     id: number;
     url: string;
+    title?: string;
 }
