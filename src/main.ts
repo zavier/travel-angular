aa70a0fdbc1cdd1a4c660d62e3588992b1441f4e
zheng <765324639@qq.com>
2019-09-02 22:21:50 +0800

    header

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import fastClick from 'fastclick'
+
+fastClick.attach(document.body);
+
 if (environment.production) {
   enableProdMode();
 }
