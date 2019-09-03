6dcaba26514da0099469461e63406266bd592721
zheng <765324639@qq.com>
2019-09-03 22:56:17 +0800

    complete icons

---
+++ src/app/icons.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IconsService {
+
+  constructor() { }
+
+  getIconPages() {
+    var iconList = [
+      {
+        id: 1,
+        url: "http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png",
+        desc: "景点门票"
+      }, {
+        id: 2,
+        url: "http://img1.qunarzz.com/piao/fusion/1711/df/86cbcfc533330d02.png",
+        desc: "滑雪季"
+      }, {
+        id: 3,
+        url: "http://img1.qunarzz.com/piao/fusion/1710/a6/83f636bd75ae6302.png",
+        desc: "泡温泉"
+      }, {
+        id: 4,
+        url: "http://img1.qunarzz.com/piao/fusion/1611/35/2640cab202c41b02.png",
+        desc: "动植园"
+      }, {
+        id: 5,
+        url: "http://img1.qunarzz.com/piao/fusion/1611/d0/e09575e66f4aa402.png",
+        desc: "游乐园"
+      }, {
+        id: 6,
+        url: "http://img1.qunarzz.com/piao/fusion/1611/59/569d3c096e542502.png",
+        desc: "必游榜单"
+      }, {
+        id: 7,
+        url: "http://img1.qunarzz.com/piao/fusion/1611/17/4bd370f3eb1acd02.png",
+        desc: "演出"
+      }, {
+        id: 8,
+        url: "http://img1.qunarzz.com/piao/fusion/1611/7f/b1ea3c8c7fb6db02.png",
+        desc: "城市观光"
+      }, {
+        id: 9,
+        url: "http://img1.qunarzz.com/piao/fusion/1611/d0/e09575e66f4aa402.png",
+        desc: "游乐园"
+      }
+    ]
+    var pages = []
+    iconList.forEach((item, index) => {
+      const page = Math.floor(index / 8)
+      if (!pages[page]) {
+        pages[page] = []
+      }
+      pages[page].push(item)
+    })
+    console.log(JSON.stringify(pages))
+    return pages;
+  }
+}
--- src/app/icons/icons.component.ts
+++ src/app/icons/icons.component.ts
@@ -1,53 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import Swiper from 'swiper';
+import { IconsService } from '../icons.service';
 
 @Component({
   selector: 'app-icons',
   templateUrl: './icons.component.html',
   styleUrls: ['./icons.component.styl']
 })
-export class IconsComponent implements OnInit {
+export class IconsComponent implements AfterViewInit {
+  pages;
 
-  iconList;
-
-  constructor() {
-    this.iconList = [
-      {
-        id: 1,
-        url: "http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png",
-        desc: "景点门票"
-      }, {
-        id: 2,
-        url: "http://img1.qunarzz.com/piao/fusion/1711/df/86cbcfc533330d02.png",
-        desc: "滑雪季"
-      }, {
-        id: 3,
-        url: "http://img1.qunarzz.com/piao/fusion/1710/a6/83f636bd75ae6302.png",
-        desc: "泡温泉"
-      }, {
-        id: 4,
-        url: "http://img1.qunarzz.com/piao/fusion/1611/35/2640cab202c41b02.png",
-        desc: "动植园"
-      }, {
-        id: 5,
-        url: "http://img1.qunarzz.com/piao/fusion/1611/d0/e09575e66f4aa402.png",
-        desc: "游乐园"
-      }, {
-        id: 6,
-        url: "http://img1.qunarzz.com/piao/fusion/1611/59/569d3c096e542502.png",
-        desc: "必游榜单"
-      }, {
-        id: 7,
-        url: "http://img1.qunarzz.com/piao/fusion/1611/17/4bd370f3eb1acd02.png",
-        desc: "演出"
-      }, {
-        id: 8,
-        url: "http://img1.qunarzz.com/piao/fusion/1611/7f/b1ea3c8c7fb6db02.png",
-        desc: "城市观光"
-      }
-    ]
+  constructor(private iconsService: IconsService) {
+    this.pages = this.iconsService.getIconPages();
   }
 
-  ngOnInit() {
+  ngAfterViewInit() {
   }
 
 }
