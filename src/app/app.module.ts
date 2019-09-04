cb036df393bf1fa231416e57ac96855d3775af75
zheng <765324639@qq.com>
2019-09-04 21:29:49 +0800

    add weekend component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HeaderComponent } from './header/header.component';
 import { SwiperComponent } from './swiper/swiper.component';
 import { IconsComponent } from './icons/icons.component';
 import { RecommendComponent } from './recommend/recommend.component';
+import { WeekendComponent } from './weekend/weekend.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { RecommendComponent } from './recommend/recommend.component';
     HeaderComponent,
     SwiperComponent,
     IconsComponent,
-    RecommendComponent
+    RecommendComponent,
+    WeekendComponent
   ],
   imports: [
     BrowserModule,
