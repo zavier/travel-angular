4b9d4918146ca84e6562e49145dbb64f3adf6fbb
zheng <765324639@qq.com>
2019-09-06 11:19:39 +0800

    fix use better-scroll

--- src/app/city/city-index/city-index.component.ts
+++ src/app/city/city-index/city-index.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import BScroll from 'better-scroll';
 
 @Component({
   selector: 'app-city-index',
   templateUrl: './city-index.component.html',
   styleUrls: ['./city-index.component.styl']
 })
-export class CityIndexComponent implements OnInit {
+export class CityIndexComponent implements AfterViewInit {
+  @ViewChild('wrapper', {static: false})  wrapperEl: ElementRef;
+  @ViewChild('headerSearch', {static: false})  headerSearchEl: ElementRef;
 
-  constructor() { }
-
-  ngOnInit() {
+  ngAfterViewInit(): void {
+    let headerSearchHeight = this.headerSearchEl.nativeElement.clientHeight;
+    let viewHeight = document.documentElement.clientHeight;
+    this.wrapperEl.nativeElement.style.height = (viewHeight - headerSearchHeight) + 'px';
+    this.scroll = new BScroll(this.wrapperEl.nativeElement, {click: true});
   }
+
+  public scroll;
+
+  constructor() {
+  }
+  
 
 }
--- src/app/city/city-list/city-list.component.ts
+++ src/app/city/city-list/city-list.component.ts
@@ -1,5 +1,4 @@
 import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
-import BScroll from 'better-scroll';
 
 @Component({
   selector: 'app-city-list',
@@ -7,13 +6,9 @@ import BScroll from 'better-scroll';
   styleUrls: ['./city-list.component.styl']
 })
 export class CityListComponent implements AfterViewInit {
-  @ViewChild('wrapper', {static: false})  wrapperEl: ElementRef;
 
-  public scroll;
 
   ngAfterViewInit(): void {
-    console.log(this.wrapperEl.nativeElement)
-    this.scroll = new BScroll(this.wrapperEl.nativeElement, {click: true});
   }
 
 
